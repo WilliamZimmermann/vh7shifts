9db59471d579f8730b270d649dd7decdb53f80d0
William Zimmermann <williamzimmermann@MacBook-Pro-de-William-2.local>
2018-06-23 17:19:45 -0300

    Now, system is getting API data from company or location and saving it

--- timetrack/tests.py
+++ timetrack/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
-from timetrack.models import Company, Location
+from timetrack.models import Company, Location, AutoBreakRules
 
 
 class CompanyTestCase(TestCase):
@@ -16,6 +16,11 @@ class CompanyTestCase(TestCase):
 
         self.assertEqual(mc_donalds.name, 'McDonalds')
         self.assertEqual(bk.name, 'BurgerKing')
+
+    def test_api_create_success(self):
+        # Test if API that gets companies is working
+        response = self.client.get('/timetrack/api/companies/get')
+        self.assertEqual(response.status_code, 200)
 
 
 class CompanyLocationTestCase(TestCase):
@@ -65,4 +70,40 @@ class CompanyLocationTestCase(TestCase):
         location_b = Location.objects.get(id=25453)
         self.assertEqual(location_a.address, '7shifts Brazil')
         self.assertEqual(location_b.address, '90 Street')
+
+
+class AutobreakRulesTestCase(TestCase):
+    def setUp(self):
+        company = Company.objects.create(name="StarBucks")
+        location = Location.objects.create(
+            id=25753,
+            company=company,
+            address="7shifts Brazil",
+            city="São Paulo",
+            country="US",
+            lat="34.0522342",
+            lng="-118.2436849",
+            state="CA",
+            timezone="America/Sao_Paulo",
+            dailyOvertimeMultiplier=1.5,
+            dailyOvertimeThreshold=480,
+            overtime=True,
+            weeklyOvertimeMultiplier=2,
+            weeklyOvertimeThreshold=2400
+        )
+        AutoBreakRules.objects.create(
+            location=location,
+            break_length=30,
+            threshold=480
+        )
+        AutoBreakRules.objects.create(
+            location=location,
+            break_length=39,
+            threshold=480
+        )
+
+    def test_settingsExists(self):
+        # Tests if settings really exists
+        abr = AutoBreakRules.objects.get(location_id=25753, break_length=39)
+        self.assertEqual(abr.threshold, 480)
 
--- timetrack/views.py
+++ timetrack/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+
+import requests
+
+from timetrack.models import Company, Location, AutoBreakRules
+
+
+def api_get_set_companies_and_settings(request):
+    # This function just get companies from API
+    can_go = True
+    status = 200
+    url = "https://shiftstestapi.firebaseio.com/locations.json"
+    json_data = requests.get(url).json()
+    data = json.dumps(json_data["25753"])
+    data = json.loads(data)
+
+    # As we doesn't have a company, I will just create a fake company
+    # Check if it was already created
+    try:
+        company_obj = Company.objects.get(id=25753)
+    except Company.DoesNotExist:
+        company_obj = Company.objects.create(
+            id=25753,
+            name="7Shifts",
+        )
+        try:
+            company_obj.save()
+        except Exception as e:
+            print("Something bad happens... Take a look: ", e)
+            can_go = False
+
+    if can_go:
+        # Save company location
+        location = Location.objects.create(
+            address=data["address"],
+            country=data["country"],
+            state=data["state"],
+            city=data["city"],
+            lat=data["lat"],
+            lng=data["lng"],
+            timezone=data["timezone"],
+            auto_break=bool(data["labourSettings"]["autoBreak"]),
+            dailyOvertimeMultiplier=data["labourSettings"]["dailyOvertimeMultiplier"],
+            dailyOvertimeThreshold=data["labourSettings"]["dailyOvertimeThreshold"],
+            overtime=data["labourSettings"]["overtime"],
+            weeklyOvertimeMultiplier=data["labourSettings"]["weeklyOvertimeMultiplier"],
+            weeklyOvertimeThreshold=data["labourSettings"]["weeklyOvertimeThreshold"],
+            company=company_obj
+        )
+
+        try:
+            location.save()
+            message = "ok"
+        except Exception as e:
+            message = "Houston we've got a problem. Something goes wrong when we tried to save the database. Take a look: " + str(e)
+            status = 404
+            can_go = False
+    else:
+        message = "Houston we've got a problem. Something goes wrong when we tried to save to the database"
+        status = 404
+
+    if can_go:  # So, get settings
+        autobreak_rules = json.dumps(data["labourSettings"]["autoBreakRules"])
+        autobreak_rules = json.loads(autobreak_rules)
+        for rule in autobreak_rules:
+            new_rule = AutoBreakRules.objects.create(
+                break_length=rule["breakLength"],
+                threshold=rule["threshold"],
+                location=location
+            )
+            try:
+                new_rule.save()
+            except Exception as e:
+                message = "Houston we've got a problem. Something goes wrong when we tried to save the database. Take a look: " + str(
+                    e)
+                status = 404
+                break
+
+    return HttpResponse(message, status=status)
