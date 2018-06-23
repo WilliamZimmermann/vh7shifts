2b5acebbff5716b518e53816152bf574d5aa79c5
William Zimmermann <williamzimmermann@MacBook-Pro-de-William-2.local>
2018-06-23 14:31:08 -0300

    Implemented new fields and I also did a test adjustment

--- timetrack/models.py
+++ timetrack/models.py
@@ -5,8 +5,13 @@ class Company(models.Model):
     # This model is the responsible to receive all companies data
     name = models.CharField(max_length=100)
 
+    # Created at and Updated ad automatic values
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __self__(self):
         return self.name
+
 
 class Location(models.Model):
     # This model is the responsible to receive all branches or companies locations
@@ -14,10 +19,21 @@ class Location(models.Model):
     address = models.CharField(max_length=200)
     country = models.CharField(max_length=3)
     state = models.CharField(max_length=5)
+    city = models.CharField(max_length=100)
     lat = models.CharField(max_length=20)
     lng = models.CharField(max_length=20)
     timezone = models.CharField(max_length=50)
     auto_break = models.BooleanField(default=0)
+
+    dailyOvertimeMultiplier = models.DecimalField(max_digits=5, decimal_places=2)
+    dailyOvertimeThreshold = models.SmallIntegerField()
+    overtime = models.BooleanField()
+    weeklyOvertimeMultiplier = models.SmallIntegerField()
+    weeklyOvertimeThreshold = models.IntegerField()
+
+    # Created at and Updated ad automatic values
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.address
@@ -28,6 +44,10 @@ class LocationLabourSettings(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     break_length = models.SmallIntegerField()
     threshold = models.SmallIntegerField()
+
+    # Created at and Updated ad automatic values
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.location
--- timetrack/tests.py
+++ timetrack/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
-from timetrack.models import Company
+from timetrack.models import Company, Location
 
 
 class CompanyTestCase(TestCase):
@@ -11,8 +11,58 @@ class CompanyTestCase(TestCase):
 
     def test_company_exists(self):
         #  This test will check if company really exists
-        mcDonalds = Company.objects.get(name="McDonalds")
+        mc_donalds = Company.objects.get(name="McDonalds")
         bk = Company.objects.get(name="BurgerKing")
 
-        self.assertEqual(mcDonalds.name, 'McDonalds')
+        self.assertEqual(mc_donalds.name, 'McDonalds')
         self.assertEqual(bk.name, 'BurgerKing')
+
+
+class CompanyLocationTestCase(TestCase):
+    # Tests for the model Company
+
+    def setUp(self):
+        # Set up 2 companies to make tests
+        company = Company.objects.create(name="StarBucks")
+        Location.objects.create(
+            id=25753,
+            company=company,
+            address="7shifts Brazil",
+            city="São Paulo",
+            country="US",
+            lat="34.0522342",
+            lng="-118.2436849",
+            state="CA",
+            timezone="America/Sao_Paulo",
+            dailyOvertimeMultiplier=1.5,
+            dailyOvertimeThreshold=480,
+            overtime=True,
+            weeklyOvertimeMultiplier=2,
+            weeklyOvertimeThreshold=2400
+        )
+
+        company = Company.objects.create(name="Wolly")
+        Location.objects.create(
+            id=25453,
+            company=company,
+            address="90 Street",
+            city="Saskatchwan",
+            country="US",
+            lat="34.0522342",
+            lng="-118.2436849",
+            state="CA",
+            timezone="America/Canada",
+            dailyOvertimeMultiplier=2.0,
+            dailyOvertimeThreshold=400,
+            overtime=True,
+            weeklyOvertimeMultiplier=2,
+            weeklyOvertimeThreshold=2400
+        )
+
+    def test_location_exists(self):
+        # Test if 2 created locations really exists in database
+        location_a = Location.objects.get(id=25753)
+        location_b = Location.objects.get(id=25453)
+        self.assertEqual(location_a.address, '7shifts Brazil')
+        self.assertEqual(location_b.address, '90 Street')
+
